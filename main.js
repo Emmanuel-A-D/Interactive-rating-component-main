ee658cd31cc17662809c7837333f9571c6d7f286
Emmanuel-A-D <emanuelalvarezdelgado@gmail.com>
2022-09-26 17:19:29 -0500

    Ajuste final en el hover del rating en el main.js

--- main.js
+++ main.js
@@ -1,5 +1,68 @@
 const container = document.querySelector ('.container');
 const btns = document.querySelectorAll ('.btn');
+const boton1 = document.querySelector ('.btn1');
+const boton2 = document.querySelector ('.btn2');
+const boton3 = document.querySelector ('.btn3');
+const boton4 = document.querySelector ('.btn4');
+const boton5 = document.querySelector ('.btn5');
+
+boton1.addEventListener("mouseover",  (e) => {
+  boton2.style.background = 'hsl(216, 12%, 54%)';
+  boton2.style.color = '#fff';
+  boton1.addEventListener("mouseout", (e) => {
+  boton2.style.background = '';
+  boton2.style.color = '';
+  });
+});
+
+boton2.addEventListener("mouseover",  (e) => {
+  boton1.style.background = 'hsl(216, 12%, 54%)';
+  boton3.style.background = 'hsl(216, 12%, 54%)';
+  boton1.style.color = '#fff';
+  boton3.style.color = '#fff';
+  boton2.addEventListener("mouseout", (e) => {
+    boton1.style.background = '';
+    boton3.style.background = '';
+    boton1.style.color = '';
+    boton3.style.color = '';
+  });
+});
+
+boton3.addEventListener("mouseover",  (e) => {
+  boton2.style.background = 'hsl(216, 12%, 54%)';
+  boton4.style.background = 'hsl(216, 12%, 54%)';
+  boton2.style.color = '#fff';
+  boton4.style.color = '#fff';
+  boton3.addEventListener("mouseout", (e) => {
+    boton2.style.background = '';
+    boton4.style.background = '';
+    boton2.style.color = '';
+    boton4.style.color = '';
+  });
+});
+
+boton4.addEventListener("mouseover",  (e) => {
+  boton3.style.background = 'hsl(216, 12%, 54%)';
+  boton5.style.background = 'hsl(216, 12%, 54%)';
+  boton3.style.color = '#fff';
+  boton5.style.color = '#fff';
+  boton4.addEventListener("mouseout", (e) => {
+    boton3.style.background = '';
+    boton5.style.background = '';
+    boton3.style.color = '';
+    boton5.style.color = '';
+  });
+});
+
+boton5.addEventListener("mouseover",  (e) => {
+  boton4.style.background = 'hsl(216, 12%, 54%)';
+  boton4.style.color = '#fff';
+  boton5.addEventListener("mouseout", (e) => {
+  boton4.style.background = '';
+  boton4.style.color = '';
+  });
+});
+
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", (e) => {
