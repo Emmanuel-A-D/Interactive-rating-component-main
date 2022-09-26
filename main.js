f088269590abd33880dce859dcf894912928a691
Emmanuel-A-D <emanuelalvarezdelgado@gmail.com>
2022-09-25 20:27:02 -0500

    Avances en el codigo js y ajuste de html

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 const container = document.querySelector ('.container');
-const submit = document.getElementById ('submit');
+const btns = document.querySelectorAll ('.btn');
 
-/* rating.addEventListener ("click", () => {
- let val ='';
-}) */
-
-submit.addEventListener ("click", () => {
- let rating = document.querySelectorAll ('.btn');
- /* for (date in rating) {
-  let value = rating[date].value;
- } */
- let htmlCode= `<section class="thank-you">
- <img src="images/illustration-thank-you.svg" alt="img-thank"> <br>
- <p id="rating"> You selected 4 out of 5</p>
- <h1 class="thank-you-h1">Thank you!</h1>
- <p class="thank-you-p">We appreciate you taking the time to give a rating. If you ever need more support, 
-   don’t hesitate to get in touch!</p>
- </section>`
- container.innerHTML = htmlCode;
-})
-
-console.log(submit);
+btns.forEach(function (btn) {
+  btn.addEventListener("click", (e) => {
+    const dato = e.currentTarget.classList;
+    if (dato.contains("btn1")) {
+      rating = 1;
+    }
+    if (dato.contains('btn2')) {
+      rating = 2;
+    }
+    if (dato.contains('btn3')) {
+      rating = 3;
+    }
+    if (dato.contains('btn4')) {
+      rating = 4;
+    } 
+    if (dato.contains('btn5')) {
+      rating = 5;
+    } 
+    if (dato.contains('submit')) {
+      value = rating;
+      if (value >= 1 && value <=5) {
+        let htmlCode= `<section class="thank-you">
+          <img src="images/illustration-thank-you.svg" alt="img-thank"> <br>
+          <p id="rating"> You selected ${value} out of 5</p>
+          <h1 class="thank-you-h1">Thank you!</h1>
+          <p class="thank-you-p">We appreciate you taking the time to give a rating. If you ever need more support, 
+          don’t hesitate to get in touch!</p>
+          </section>`;
+          container.innerHTML = htmlCode;
+      }
+    } 
+  })
+});
